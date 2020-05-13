4ac093556aba6701ac15c5118621ef477229273d
andre lawrence <andrelawrence35@gmail.com>
2020-05-13 13:03:01 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
-#write your code here
-
-def countdown
+x = 10
+def countdown(x)
+ while x > 0
+  puts "#{x} SECOND(S)!"
+  x-= 1
 end
+return "HAPPY NEW YEAR!"
+ end 
+   
+x= 5
+def countdown_with_sleep(x)
+  while x > 0 
+  puts "#{x} SECOND(S)!"
+  sleep 1
+  x-= 1
+  
+  
+end 
+
+end 
